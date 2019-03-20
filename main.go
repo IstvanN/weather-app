4879de556b7360fe57aae87fdd7f6487054c842a
Istvan Nemeth <nemethistvan.ius@gmail.com>
2019-03-20 20:25:12 +0100

    feat(main): add the basics of http server

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const (
+	port = ":8080"
+)
 
 func main() {
-	fmt.Println("Hello, weather!")
+	http.HandleFunc("/", indexHandler)
+	log.Println("app is up and running on", port)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello weather!")
 }
