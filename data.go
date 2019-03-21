e7b786de50e3788c2cea1c0622ed5932335ca47a
Istvan Nemeth <nemethistvan.ius@gmail.com>
2019-03-21 18:54:04 +0100

    fix(data): restructure rawdata as struct

--- data.go
+++ data.go
@@ -2,33 +2,41 @@ package main
 
 var wd WeatherData
 
-type rawData []WeatherData
+type rawData struct {
+	ConsolidatedWeather []struct {
+		State          string  `json:"weather_state_name"`
+		MinTemp        float64 `json:"min_temp"`
+		MaxTemp        float64 `json:"max_temp"`
+		ActualTemp     float64 `json:"the_temp"`
+		Predictability int     `json:"predictability"`
+	} `json:"consolidated_weather"`
+	Title string `json:"title"`
+}
 
 // WeatherData is the aggregated data structure for the received JSON format of the API
 type WeatherData struct {
-	CityName       string `json:"title"`
-	State          string `json:"weather_state_name"`
-	MinTemp        int    `json:"min_temp"`
-	MaxTemp        int    `json:"max_temp"`
-	ActualTemp     int    `json:"the_temp"`
-	Predictability int    `json:"predictability"`
+	CityName       string  `json:"title"`
+	State          string  `json:"weather_state_name"`
+	MinTemp        float64 `json:"min_temp"`
+	MaxTemp        float64 `json:"max_temp"`
+	ActualTemp     float64 `json:"the_temp"`
+	Predictability int     `json:"predictability"`
 }
 
 func getHighestPredict(raw rawData) WeatherData {
 	var valueOfHighestPredict int
 	var indexOfHighestPredict int
 
-	for i, wd := range raw {
+	for i, wd := range raw.ConsolidatedWeather {
 		if wd.Predictability > valueOfHighestPredict {
 			valueOfHighestPredict = wd.Predictability
 			indexOfHighestPredict = i
 		}
 	}
 
-	highestPredictData := raw[indexOfHighestPredict]
+	highestPredictData := raw.ConsolidatedWeather[indexOfHighestPredict]
 
 	highestWeatherData := WeatherData{
-		CityName:       highestPredictData.CityName,
 		State:          highestPredictData.State,
 		MinTemp:        highestPredictData.MinTemp,
 		MaxTemp:        highestPredictData.MaxTemp,
